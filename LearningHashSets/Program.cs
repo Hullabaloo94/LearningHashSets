c6bbfeb4f2c4ea7f3c41ba8a1252311b9df6ff49
Jason Hall <jasonahall1994@gmail.com>
2019-11-14 12:33:30 +0000

    initial commit

---
+++ LearningHashSets/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningHashSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Hash sets help remove duplicate elements in an array.
+            var myHash = new HashSet<String>();
+            myHash.Add("hello");
+            myHash.Add("hello");
+
+            Console.WriteLine(myHash.Count);
+
+            //Overlaps can also be used in Hashsets to see if any of the elements in the two compared arrays are the same.
+            String[] s = new string[] { "hello", "Goodbye" };
+            // This will show true because "hello" is in both myHash and the array named 's'.
+            Console.WriteLine(myHash.Overlaps(s));
+        }
+    }
+}
